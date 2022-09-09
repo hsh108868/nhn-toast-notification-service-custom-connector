d202cb159f6dbb2c96b6c555c2575ec8b595df97
SangminPark <37584765+MultiDCP@users.noreply.github.com>
2022-09-09 20:03:09 +0900

    Add Essential Code for Startup and Provider

--- src/nt-ping/PingTriggerFunctionProvider.cs
+++ src/nt-ping/PingTriggerFunctionProvider.cs
@@ -1,12 +1,20 @@
+using Microsoft.Azure.WebJobs.Script.Description;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NhnToast.Ping
 {
-    public class PingTriggerFunctionProvider
+    public class PingTriggerFunctionProvider : IFunctionProvider
     {
+        public ImmutableDictionary<string, ImmutableArray<string>> FunctionErrors { get; } = new Dictionary<string, ImmutableArray<string>>().ToImmutableDictionary();
+
+        public Task<ImmutableArray<FunctionMetadata>> GetFunctionMetadataAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/nt-ping/PingWebJobsStartup.cs
+++ src/nt-ping/PingWebJobsStartup.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Resolvers;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.WebJobs.Script.Description;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace NhnToast.Ping
 {
     public class PingWebJobsStartup
     {
+        public void ConfigureServices(IWebJobsBuilder builder)
+        {
+            builder.Services.AddHealthChecks();
+        }
     }
 }
